33623b11dd7f400e0057a8e1e909bcc41650bdcd
UlviAlakbarli <46805918+UlviAlakbarli@users.noreply.github.com>
2019-05-05 01:16:59 +0400

    Update 2

--- js/main.js
+++ js/main.js
@@ -1,1 +1,23 @@
 "use strict";
+//slider
+$(document).ready(function(){
+let sliderLength=document.querySelectorAll(".slide li").length;
+let sliderIndex=0;
+$(".left-arrow").click(
+    function(){
+      sliderIndex++;
+       if(sliderIndex>0){
+           sliderIndex=(-sliderLength)+1;
+       }
+       $(".slide").css({"left":`${(sliderIndex)*100}%`});
+    }
+)
+$(".right-arrow").click(function(){
+      sliderIndex--;
+      if(sliderIndex===(-sliderLength)){
+          sliderIndex=0;
+      }
+      $(".slide").css({"left":`${(sliderIndex)*100}%`});
+})
+
+})
