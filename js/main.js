7f973b1fa16f9597bd4803dfe97200f206654fb9
UlviAlakbarli <46805918+UlviAlakbarli@users.noreply.github.com>
2019-05-05 18:30:17 +0400

    #3

--- js/main.js
+++ js/main.js
@@ -20,4 +20,73 @@ $(".right-arrow").click(function(){
       $(".slide").css({"left":`${(sliderIndex)*100}%`});
 })
 
+});
+// HOVER LI LEFT
+"use strict"
+$(document).ready(function(){
+    let hoverLeftLi=$(".hoverLeftLi");
+    hoverLeftLi.hover(
+        function(){
+         let a=$(this).children()[0]
+         a.style.left="80%";
+         a.style.opacity="0";
+            setTimeout(function(){
+                a.style.left="20%";
+                setTimeout(function(){
+                    a.style.opacity="1";
+                    a.style.left="50%";
+                },190)
+                
+            }, 190);
+            
+         },
+         function(){}
+       
+    )
+});
+"use strict"
+$(document).ready(function(){
+    let toTopButton=$(".toTopButton");
+    toTopButton.hover(
+        function(){
+         let a=$(this).children()[0]
+         a.style.top="20%";
+         a.style.opacity="0";
+            setTimeout(function(){
+                a.style.top="80%";
+                setTimeout(function(){
+                    a.style.opacity="1";
+                    a.style.top="50%";
+                },190)
+                
+            }, 190);
+            
+         },
+         function(){}
+       
+    )
+
+
+    
+    toTopButton.click(function(){
+        
+       let body= document.querySelector("body");
+       console.log(body.scrollHeight);
+       let scrollh=body.scrollHeight;
+       
+      let d= setInterval (function(){
+        
+        console.log("salam");
+
+        scrollh=scrollh-15;
+          
+        window.scrollTo(0,(scrollh));
+        if(scrollh<0){
+            clearInterval(d);
+        }
+        
+  
+       },5)
+      
+    })
 })
